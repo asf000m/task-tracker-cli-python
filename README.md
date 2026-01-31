0689348f411d2f9d5f0f9a0090f40967b5b2e509
asf000m <asf000m@gmail.com>
2026-01-31 17:41:06 -0300

    Finish add() function for the add command

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Task Tracker
 Build a CLI app to track your tasks and manage your to-do list.
 
+Each task has an ID.
+Adding a task should:
+    - add to the list of tasks a new ID and the task.
--- taskcli/__main__.py
+++ taskcli/__main__.py
@@ -1,8 +1,24 @@
 import sys
+import json
+import datetime
 
-def add():
-    """ Add a new task """
-    return 0
+def add(task, description, task_list):
+    """ Add a new task to the list"""
+
+    new_id = str(len(task_list) + 1)    # create id number of the new task
+
+    now = datetime.datetime.now().strftime('%c')
+
+    # add new task and its properties
+    task_list[new_id] = {
+        'task': task,
+        'id': int(new_id),
+        'description': description,
+        'status': 'todo',
+        'createdAt': now,
+        'updatedAt': now
+    }
+    return task_list
 
 def update():
     """ Change a task in the list to another """
@@ -29,15 +45,26 @@ def listByStatus():
     return 0
 
 def main():
-    print('in main...')
 
-    args = sys.argv[1:]
+    # read JSON
+    some_json = '{}'
+    task_list = {}
 
-    print('count of args :: {}'.format(len(args)))
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
+    # define the command and arguments
+    command = sys.argv[1]
+    args = sys.argv[2:]
 
-    print('Hello world!')
+    # check for a command and execute it
+    if command == 'list':
+        for task in task_list:
+            print(task['task'], task['description'])
+    elif command == 'add':
+        task_list = add(args[0], args[1], task_list)
+
+    # write to JSON
+    some_json = json.dumps(task_list)
+    print(some_json)
+
 
 if __name__ == '__main__':
     main()
