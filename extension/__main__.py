7205021ae053ff20aa37464e88c69e9fa9213984
asf000m <asf000m@gmail.com>
2026-01-31 16:17:57 -0300

    Change directories, file names, and structure

---
+++ extension/__main__.py
@@ -0,0 +1,30 @@
+def add():
+    """ Add a new task """
+    return 0
+
+def update():
+    """ Change a task in the list to another """
+    return 0
+
+def delete():
+    """ Delete a task """
+    return 0
+
+def markInProgress():
+    """ Mark a task as in progress """
+    return 0
+
+def markDone():
+    """ Mark a task as done """
+    return 0
+
+def listAll():
+    """ List all tasks """
+    return 0
+
+def listByStatus():
+    """ List tasks by status """
+    return 0
+
+def main():
+    
